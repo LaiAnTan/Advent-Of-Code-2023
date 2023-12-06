2f3bfbc25897f73037cdd02b039a6c14825eaac6
laian <tanlaian2004@gmail.com>
2023-12-06 18:07:16 +0800

    i cannot

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Learning a new language (JavaScript) through 25 days of programming problems.
 
-Puzzle questions / inputs can be found ![here](https://adventofcode.com/2023).
+Puzzle questions / inputs can be found [here](https://adventofcode.com/2023).
--- day05/day05_part2.js
+++ day05/day05_part2.js
@@ -11,7 +11,7 @@ function get_seed_ranges(data)
 
 	for (let i = 0; i < values.length; i += 2)
 	{
-		seed_ranges.push([values[i], values[i + 1]])
+		seed_ranges.push([values[i], values[i] + values[i + 1]])
 	}
 	return (seed_ranges)
 }
@@ -36,113 +36,45 @@ function parse_maps(data)
 	return (maps_arr)
 }
 
-/*
-map_range
-0 - dest range start
-1 - src range start
-2 - range size
-
-seed_range
-0 - range start
-1 - range size
-*/
-
-// converts the range into 1, 2 or 3 ranges depending on overlap
-function convert_range(src_range, map_range)
-{
-	let converted = []
-	let not_converted = []
-	let new_range
-
-	if (map_range[1] <= src_range[0] && (src_range[0] + src_range[1]) <= (map_range[1] + map_range[2])) 
-	{
-		//console.log("src is totally inside map", src_range, map_range)
-		// if src is totally inside map, move the whole range
-		new_range = [map_range[0] + src_range[0] - map_range[1], src_range[1]]
-		converted.push(new_range)
-	}
-	else if (src_range[0] < map_range[1] && (map_range[1] + map_range[2]) < (src_range[0] + src_range[1]))
-	{
-		//console.log("map is totally inside src", src_range, map_range)
-		// if map is totally inside src, split int 3 ranges
-		new_range = [src_range[0], map_range[1] - src_range[0]] // not converted left
-		not_converted.push(new_range)
-		new_range = [map_range[1] + map_range[2], src_range[0] + src_range[1]] // not converted right
-		not_converted.push(new_range)
-		new_range = [map_range[0], map_range[2]] // converted middle
-		converted.push(new_range)
-	}
-	else if (map_range[1] < src_range[0] && src_range[0] < (map_range[1] + map_range[2]) && (map_range[1] + map_range[2]) < (src_range[0] + src_range[1]))
-	{
-		//console.log("left side of src is inside map", src_range, map_range)
-		// if only the left side of src is inside map, move the left side and copy the right
-		new_range = [map_range[0] + src_range[0] - map_range[1], map_range[1] + map_range[2] - src_range[0]] // converted left
-		converted.push(new_range)
-		new_range = [map_range[1] + map_range[2], (src_range[0] + src_range[1]) - (map_range[1] + map_range[2])] // not converted right
-		not_converted.push(new_range)
-	}
-	else if (src_range[0] < map_range[1] && map_range[1] < (src_range[0] + src_range[1]) && (src_range[0] + src_range[1]) < (map_range[1] + map_range[2]))
-	{
-		//console.log("right side of src is inside map", src_range, map_range)
-		// if only the right side of src is inside map, move the right side and copy the left
-		new_range = [map_range[0], src_range[0] + src_range[1] - map_range[1]] // converted right
-		converted.push(new_range)
-		new_range = [src_range[0], map_range[1] - src_range[0]] // not converted left
-		not_converted.push(new_range)
-	}
-	else
-	{
-		//console.log("map total miss")
-	}
-	// console.log("Converted:", converted)
-	// console.log("Not converted:", not_converted)
-	return ([converted, not_converted])
-}
-
 // we must convert whole ranges at a time because brute force is too slow
+// credit: hypernutrino
 function convert_ranges(seed_ranges, maps)
 {
+	let not_converted
 	let ranges = seed_ranges
+
+	console.log("Ranges:", ranges)
 
 	for (let map of maps)
 	{
-		console.log("Current Map:", map)
-		console.log("Ranges before conversion:", ranges)
-
-		let output
 		let converted = []
-		let not_converted = []
-		let ranges_copy = [...ranges]
-		for (let map_range of map)
+		while (ranges.length > 0)
 		{
-			//console.log("Current map range:", map_range)
-			//console.log("Current ranges", ranges)
-			for (let i = 0; i < ranges.length; ++i)
+			let curr = ranges.pop()
+			not_converted = true
+			for (let map_range of map)
 			{
-				//console.log("Trying to convert:", ranges[i])
-
-				output = convert_range(ranges[i], map_range) // output[0] = converted, output[1] = not converted
-
-				converted = converted.concat(output[0])
-				not_converted = not_converted.concat(output[1])
-
-				// full miss
-				if (output[0].length === 0 && output[1].length === 0)
+				let overlap_start = Math.max(curr[0], map_range[1])
+				let overlap_end = Math.min(curr[1], map_range[1] + map_range[2])
+				if (overlap_start < overlap_end) // there is an overlap
 				{
-					//console.log("Conversion fail")
-					continue
+					not_converted = false
+					converted.push([overlap_start - map_range[1] + map_range[0], overlap_end - map_range[1] + map_range[0]])
+					if (overlap_start > curr[0]) // there is excess on the left
+					{
+						ranges.push([curr[0], overlap_start])
+					}
+					if (curr[1] > overlap_end) // there is excess on the right
+					{
+						ranges.push([overlap_end, curr[1]])
+					}
+					break
 				}
-
-				//console.log("Conversion success, removing", ranges[i])
-				ranges_copy.splice(ranges_copy.findIndex(element => element === ranges[i]), 1)
-
-				// console.log("ranges_copy:", ranges_copy)
-				// console.log("Converted:", converted, "Not converted:", not_converted)
 			}
-
-			ranges = [...not_converted, ...ranges_copy]
+			if (not_converted === true) // carry over if not converted
+				converted.push(curr)
 		}
-		ranges = [...ranges, ...converted]
+		ranges = converted
 	}
 	return (ranges)
 }
